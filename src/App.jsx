bcd0efe94672bc83823f5eb5e3b3ebf89b38b89b
Akshay <akshayvaghela2763@gmail.com>
2025-09-07 22:31:17 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-function App() {
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Login from "./Login"
+import Profile from "./Profile"
+import Body from './Body'
+
+const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button
-        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
